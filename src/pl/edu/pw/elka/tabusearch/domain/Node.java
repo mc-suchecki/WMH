01f99d9373876bc08afd518683e9b81ecc5e9b11
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-11-11 13:21:26 +0100

    Added hashCode and equals method to Node

--- src/pl/edu/pw/elka/tabusearch/domain/Node.java
+++ src/pl/edu/pw/elka/tabusearch/domain/Node.java
@@ -1,15 +1,39 @@
-/**
- * @author <a href="mailto:jacek.witkowski@gmail.com">Jacek Witkowski</a>
- * Created on 2014-11-2014.
- */
 package pl.edu.pw.elka.tabusearch.domain;
+
+import java.util.Objects;
 
 public class Node
 {
     private String label;
 
+    public Node(final String label)
+    {
+        this.label = label;
+    }
+
     public String getLabel()
     {
         return label;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(label);
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final Node other = (Node) obj;
+        return Objects.equals(this.label, other.label);
     }
 }
