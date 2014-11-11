b485e3a3938f8c6384af87bdcfafa9d3edc07f84
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-11-11 13:20:25 +0100

    Added Graph class (basic implementation, without access methods)

--- src/pl/edu/pw/elka/tabusearch/domain/Graph.java
+++ src/pl/edu/pw/elka/tabusearch/domain/Graph.java
@@ -1,10 +1,16 @@
-/**
- * @author <a href="mailto:jacek.witkowski@gmail.com">Jacek Witkowski</a>
- * Created on 2014-11-2014.
- */
 package pl.edu.pw.elka.tabusearch.domain;
+
+import java.util.Map;
+import java.util.Set;
 
 public class Graph
 {
-    //TODO
+    private final Map<String,Node> labelsToNodesMap;
+    private final Set<Edge> edges;
+
+    public Graph(final Map<String,Node> labelsToNodesMap, final Set<Edge> edges)
+    {
+        this.labelsToNodesMap = labelsToNodesMap;
+        this.edges = edges;
+    }
 }
