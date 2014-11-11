9e59e2f047abf270eda63bca65bbd30bdfd233b1
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-11-11 13:28:15 +0100

    Deleted author info header from some files

--- src/pl/edu/pw/elka/tabusearch/io/ConfigReader.java
+++ src/pl/edu/pw/elka/tabusearch/io/ConfigReader.java
@@ -1,12 +1,7 @@
-/**
- * @author <a href="mailto:jacek.witkowski@gmail.com">Jacek Witkowski</a>
- * Created on 2014-11-2014.
- */
 package pl.edu.pw.elka.tabusearch.io;
 
 public class ConfigReader
 {
-
     public Config read()
     {
         return null;//TODO
--- src/pl/edu/pw/elka/tabusearch/io/ConsoleSolutionPrinter.java
+++ src/pl/edu/pw/elka/tabusearch/io/ConsoleSolutionPrinter.java
@@ -1,7 +1,3 @@
-/**
- * @author <a href="mailto:jacek.witkowski@gmail.com">Jacek Witkowski</a>
- * Created on 2014-11-2014.
- */
 package pl.edu.pw.elka.tabusearch.io;
 
 import pl.edu.pw.elka.tabusearch.domain.Node;
