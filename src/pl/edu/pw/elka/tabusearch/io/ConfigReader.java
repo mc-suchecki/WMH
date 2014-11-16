cf84bddbb527418aa0da9bcbd9d29754569205d3
Jacek Witkowski <jacek.witkowski@gmail.com>
2014-11-16 20:22:23 +0100

    Implemented ConfigurationReader

--- src/pl/edu/pw/elka/tabusearch/io/ConfigReader.java
+++ src/pl/edu/pw/elka/tabusearch/io/ConfigReader.java
@@ -1,8 +1,27 @@
 package pl.edu.pw.elka.tabusearch.io;
 
+import java.io.*;
+import java.util.Properties;
+
 public class ConfigReader {
-    public Config read() {
-        //TODO read these from file - Jacek
-        return new Config(5, 10, 2, 10);
+    private static final String CONFIG_FILE_NAME = "test/config.properties";
+
+    public Config read() throws IOException, NumberFormatException {
+        final Properties properties = loadProperties();
+        final Integer minParameter = Integer.parseInt(properties.getProperty("minParameter"));
+        final Integer maxParameter = Integer.parseInt(properties.getProperty("maxParameter"));
+        final Integer plusParameter = Integer.parseInt(properties.getProperty("plusParameter"));
+        final Integer tabuListSize = Integer.parseInt(properties.getProperty("tabuListSize"));
+
+        return new Config(minParameter, maxParameter, plusParameter, tabuListSize);
+    }
+
+    private Properties loadProperties() throws IOException {
+        final Properties prop = new Properties();
+        try (final BufferedReader br = new BufferedReader(new FileReader(CONFIG_FILE_NAME))) {
+            prop.load(br);
+        }
+
+        return prop;
     }
 }
